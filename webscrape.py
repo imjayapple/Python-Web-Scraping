3bcdffd02164df92e04fefffc71b51d1f8ab4943
imjayapple <imjayapple@gmail.com>
2023-03-23 15:29:40 -0700

    Installed prerequisites, added pip to environment variable, installed beautiful soup, installed pip, installed requests

--- webscrape.py
+++ webscrape.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+# define the URL of the blog
+url = 'https://calnewport.com/blog/'
+
+# send an HTTP GET request to the URL and get the content
+response = requests.get(url)
+
+print(response)
